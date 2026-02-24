201915ec1b71399f985fae6c361353433dd970c5
VoidXDev <developershiva18@gmail.com>
2026-02-24 13:18:04 +0530

    Delete helpers directory

--- helpers/ffmpeg.py
+++
@@ -1,8 +0,0 @@
-import os, subprocess
-
-def generate_thumbnail(video_path, thumb_path):
-    try:
-        subprocess.call(['ffmpeg', '-i', video_path, '-ss', '00:00:02.000', '-vframes', '1', '-update', '1', thumb_path, '-y'])
-        return thumb_path if os.path.exists(thumb_path) else None
-    except:
-        return None
