f1ca1bed5617639cc0e375e6c43f3c1eaab18055
VoidXDev <developershiva18@gmail.com>
2026-02-24 13:39:14 +0530

    Create config.py

---
+++ bot/config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    API_ID = int(os.getenv("API_ID"))
+    API_HASH = os.getenv("API_HASH")
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    MONGO_URL = os.getenv("MONGO_URL")
+    DUMP_CHAT_ID = int(os.getenv("DUMP_CHAT_ID"))
+    FSUB_CHANNEL = os.getenv("FSUB_CHANNEL", "") # Without @
+    OWNER_ID = "5298223577"
